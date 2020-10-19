0e4f31f75c525e1c994ca1c20dcb74d0f481c119
JSnow <esoj.nieves@gmail.com>
2020-10-19 14:16:57 +0200

    db fix

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -66,7 +66,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Luca', '2018-06-08', 2, 11);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Luna', '2011-06-04', 3, 12);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Luna', '2011-06-04', 3, 12);
 
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
