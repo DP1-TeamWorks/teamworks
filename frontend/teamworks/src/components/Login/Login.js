9afe441e266c9d14bc67e2404d8c75e94569ce98
JSnow <esoj.nieves@gmail.com>
2020-12-02 13:53:23 +0100

    Forms basic structure Implemented for the login form

--- frontend/teamworks/src/components/Login/Login.js
+++ frontend/teamworks/src/components/Login/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./login.css";
+import LoginForm from "./LoginForm"
+import {login} from "../../utils/api/authApiUtils"
 export function Login() {
   return (
     <React.Fragment>
       <div className="container">
         <div className="loginBox">
           <span className="teamWord">TEAM</span><span className="worksWord">WORKS</span>
+          <LoginForm />
         </div>
       </div>
     </React.Fragment>
