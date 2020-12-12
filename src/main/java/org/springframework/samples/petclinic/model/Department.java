3d84be6e91a82899d0121d8a68535a6ae016eab0
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2020-12-12 21:02:48 +0100

    Deparment and Team relation and relations fix

--- src/main/java/org/springframework/samples/petclinic/model/Department.java
+++ src/main/java/org/springframework/samples/petclinic/model/Department.java
@@ -4,10 +4,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.sun.istack.NotNull;
 
 import lombok.Data;
@@ -24,7 +27,13 @@ public class Department extends BaseEntity {
 	@Column(name = "description")
 	private String description;
 	
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "team_id")
+	@JsonBackReference
+	private Team team;
 	@Column(name = "projects")
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "department", orphanRemoval = true)
 	private List<Project> projects;
+	
 }
--- src/main/java/org/springframework/samples/petclinic/model/Project.java
+++ src/main/java/org/springframework/samples/petclinic/model/Project.java
@@ -1,27 +1,24 @@
 package org.springframework.samples.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.sun.istack.NotNull;
 
 import lombok.Data;
 @Data
 @Entity
-@Table(name = "projects")
+@Table(name = "project")
 public class Project extends BaseEntity{
 	
 	
@@ -41,4 +38,6 @@ public class Project extends BaseEntity{
 	@JoinColumn(name = "department_id")
 	@JsonBackReference
 	private Department department;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "project", orphanRemoval = true)
+	private List<Milestone> milestones;
 }
