420abf1855d2b1acf2abd129882e49c577f2cafc
JSnow <esoj.nieves@gmail.com>
2020-12-14 12:24:19 +0100

    Added signup to api requests

--- frontend/teamworks/src/utils/api/authApiUtils.js
+++ frontend/teamworks/src/utils/api/authApiUtils.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 import { API_URL } from '../../config/config';
 const AUTH_URL = '/auth';
 
-export const login = (credentials) => axios.post(API_URL + AUTH_URL + '/', credentials);
-export const check = () => axios.get(API_URL + AUTH_URL)
-export const logout = () => axios.delete(API_URL + AUTH_URL)
+export default {
+    login : (credentials) => axios.post(API_URL + AUTH_URL + '/' + "login" + "/", credentials),
+    signup : (props) => axios.post(API_URL + AUTH_URL + '/' + "signup" + "/", props),
+    check : () => axios.get(API_URL + AUTH_URL),
+    logout : () => axios.delete(API_URL + AUTH_URL)
+}
+
