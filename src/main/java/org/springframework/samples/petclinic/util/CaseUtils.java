834cfb73789e7831b33bc4f27e9339cfd5a7b7d7
JSnow <esoj.nieves@gmail.com>
2020-12-27 19:40:21 +0100

    case utils fix

--- src/main/java/org/springframework/samples/petclinic/util/CaseUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/CaseUtils.java
@@ -1,14 +1,10 @@
 package org.springframework.samples.petclinic.util;
 
-import java.util.stream.IntStream;
-
 public class CaseUtils {
-	public static String toPascalCase(String s){
-		  String a=s.substring(0,1).toUpperCase();
-		  String b=s.substring(1).toLowerCase();
-		  return a+b;
-		}
-
-		
+	public static String toPascalCase(String s) {
+		String a = s.substring(0, 1).toUpperCase();
+		String b = s.substring(1).toLowerCase();
+		return a + b;
+	}
 
 }
