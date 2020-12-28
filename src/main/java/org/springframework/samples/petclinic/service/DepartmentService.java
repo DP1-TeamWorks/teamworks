59379f3210a54cc3747dd26fb85096f1c005b658
JSnow <esoj.nieves@gmail.com>
2020-12-28 20:35:54 +0100

    Added create/del Belongs to Department controller

--- src/main/java/org/springframework/samples/petclinic/service/DepartmentService.java
+++ src/main/java/org/springframework/samples/petclinic/service/DepartmentService.java
@@ -1,19 +1,4 @@
 package org.springframework.samples.petclinic.service;
-/*
- * Copyright 2002-2013 the original author or authors.|
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -24,12 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
-/**
- * Mostly used as a facade for all Petclinic controllers Also a placeholder
- * for @Transactional and @Cacheable annotations
- *
- * @author Michael Isvy
- */
 @Service
 public class DepartmentService {
 	private DepartmentRepository departmentRepository;
