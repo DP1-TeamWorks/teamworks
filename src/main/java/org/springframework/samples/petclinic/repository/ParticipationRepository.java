129f9929888c7041cddc1131bc504b729f103eb8
Maria <mariacastrobonilla@gmail.com>
2020-12-29 16:45:27 +0100

    Particaption Service

--- src/main/java/org/springframework/samples/petclinic/repository/ParticipationRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/ParticipationRepository.java
@@ -1,13 +1,15 @@
 package org.springframework.samples.petclinic.repository;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.repository.Repository;
-import org.springframework.samples.petclinic.model.Belongs;
 
-public interface ParticipationRepository extends Repository<Belongs, Integer>{
-	void save(Belongs belongs) throws DataAccessException;
-	void deleteById(Integer belongsId) throws DataAccessException;
-	Belongs findById(Integer projectId) throws DataAccessException;
+import org.springframework.data.repository.Repository;
+
+import org.springframework.samples.petclinic.model.Participation;
+
+public interface ParticipationRepository extends Repository<Participation, Integer>{
+	void save(Participation participation) throws DataAccessException;
+	void deleteById(Integer participationId) throws DataAccessException;
+	Participation findById(Integer projectId) throws DataAccessException;
 	
 	
 }
