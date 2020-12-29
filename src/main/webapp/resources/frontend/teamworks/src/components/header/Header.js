4622cf9e02fc297bc397dde06bf2d79147fff849
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2020-12-29 15:30:22 +0100

    Inbox Header Config

--- src/main/webapp/resources/frontend/teamworks/src/components/header/Header.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/header/Header.js
@@ -1,5 +1,17 @@
-import "./Header.css"
+import React from "react";
+import "./Header.css";
+import "../Auth/Login/Login.css";
 
-const Header = () => <div className="Header"></div>
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="HeaderTitle">
+        <span className="TeamWord">TEAM</span>
+        <span className="WorksWord">WORKS</span>
+      </div>
+      <input className='SearchInput'></input>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
