01a2842b1884b389882236b31f93bd4b4982564e
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2020-12-29 19:07:34 +0100

    Header semi-finished

--- src/main/webapp/resources/frontend/teamworks/src/components/header/Header.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
+import "../Auth/Login/Login";
+import Button from '../buttons/Button';
+import ProfileHeader from '../profile/ProfileHeader';
 import "./Header.css";
-import "../Auth/Login/Login.css";
 
 const Header = () => {
   return (
@@ -9,8 +11,13 @@ const Header = () => {
         <span className="TeamWord">TEAM</span>
         <span className="WorksWord">WORKS</span>
       </div>
-      <input className='SearchInput'></input>
+      <label className="HeaderSearchbar">
+      <input className='SearchInput' placeholder="Search for messages"></input>
+      </label>
+      <Button className='Button--green HeaderButton'>Manage team</Button>
+      <ProfileHeader src="https://avatars0.githubusercontent.com/u/1417708?s=460&u=b8dd20ef775f23c845a418513e48617181de734a&v=4"/>
     </div>
+    
   );
 };
 
