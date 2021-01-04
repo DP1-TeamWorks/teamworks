4b7da79b3a279d938fdf6d7bd9bb615133291bca
Pedro Alonso Pontiga <pedropontiga@gmail.com>
2021-01-04 21:07:38 +0100

    Adding querys and updating participation service and repository

--- src/main/java/org/springframework/samples/petclinic/service/ParticipationService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ParticipationService.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.service;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Participation;
+import org.springframework.samples.petclinic.model.Project;
 import org.springframework.samples.petclinic.repository.ParticipationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,13 +27,21 @@ public class ParticipationService {
 	public void deleteParticipationById(Integer participationId) throws DataAccessException {
 		participationRepository.deleteById(participationId);
 	}
-
+	@Transactional(readOnly = true)
 	public Participation findParticipationById(Integer participationId) throws DataAccessException {
 		return participationRepository.findById(participationId);
 	}
-
-	public Participation findParticipationByUserIdAndProjectId(Integer userId, Integer projectId) {
-		return new Participation();
+	@Transactional(readOnly = true)
+	public Collection<Participation> findParticipationByUserIdAndProjectId(Integer userId, Integer projectId) {
+		return participationRepository.findParticipationByUserAndDeparment(userId, projectId);
 	}
-
+	
+	@Transactional(readOnly = true)
+	public Participation findCurrentParticipation(Integer userId, Integer projectId) {
+		return participationRepository.findCurrentParticipation(userId, projectId);
+	}
+	@Transactional(readOnly = true)
+	public Collection<Project> findMyProjects(Integer userId){
+		return participationRepository.findMyProjects(userId);
+	}
 }
