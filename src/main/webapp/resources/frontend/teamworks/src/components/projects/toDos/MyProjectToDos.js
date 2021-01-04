2657ab4f9c474d65e053c5e529c288660d39d6f6
JSnow <esoj.nieves@gmail.com>
2021-01-04 18:53:32 +0100

    Implemented addNewToDo (inbox sidebar)

--- src/main/webapp/resources/frontend/teamworks/src/components/projects/toDos/MyProjectToDos.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/projects/toDos/MyProjectToDos.js
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
 import ToDo from "./ToDo";
 import "./ToDos.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Input from "../../forms/Input";
 
 const MyProjectToDos = ({ myProjectToDoList }) => {
+  const [newToDo, setNewToDo] = useState("");
+
+  const addNewToDo = () => {
+    myProjectToDoList.push({
+      title: newToDo,
+      tagList: [],
+    });
+    console.log(myProjectToDoList);
+  };
+
   return (
     <>
       <h3 className="SidebarSectionTitle">ToDo</h3>
       {myProjectToDoList.map((toDo) => {
         return <ToDo title={toDo.title} tagList={toDo.tagList} />;
       })}
+      <button className="ToDoAdd" onClick={null}>
+        <FontAwesomeIcon icon={faPlus} style={{ color: "lightgray" }} />
+      </button>{" "}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(newToDo);
+          addNewToDo();
+          event.target.reset();
+          setNewToDo("");
+        }}
+        style={{ display: "inline-block" }}
+      >
+        <Input
+          placeholder="Add new ToDo ..."
+          styleClass="InputNewToDo"
+          changeHandler={(event) => {
+            setNewToDo(event.target.value);
+          }}
+        />
+      </form>
     </>
   );
 };
