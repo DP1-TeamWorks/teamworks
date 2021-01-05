8a08a01d1da2489a307f9b2b5b4aa79a5ab13421
JSnow <esoj.nieves@gmail.com>
2021-01-05 14:09:03 +0100

    Improved ToDo's markAsDone animation

--- src/main/webapp/resources/frontend/teamworks/src/components/projects/toDos/ToDo.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/projects/toDos/ToDo.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Circle from "../tags/Circle";
 import ToDoCircle from "./ToDoCircle";
 
 const ToDo = ({ tagList, title }) => {
   const [isDone, setIsDone] = useState(false);
+  const [isDoneAnimation, setIsDoneAnimation] = useState(false);
+
+  const markAsDone = () => {
+    setIsDoneAnimation(true);
+    // TODO: fix the animation
+    setTimeout(() => setIsDone(true), 700);
+    // TODO: call API request to mark as done the real TODO
+  };
 
   return (
-    <div className="TagTab">
-      <ToDoCircle isDone={isDone} setIsDone={setIsDone} /> <span>{title} </span>
+    <div className="ToDoTab" onClick={markAsDone} hidden={isDone}>
+      <ToDoCircle isDone={isDoneAnimation} markAsDone={markAsDone} />{" "}
+      <span
+        style={{ textDecoration: isDoneAnimation ? "line-through" : "none" }}
+      >
+        {title}{" "}
+      </span>
       <div style={{ float: "right" }}>
         {tagList.map((tag) => {
           return <Circle color={tag.color} />;
