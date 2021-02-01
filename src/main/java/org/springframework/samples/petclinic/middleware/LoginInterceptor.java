316e1913fb8be512db09e3accbd36200ced1340e
Nicolás de Ory <deorynicolas@gmail.com>
2021-02-01 01:26:48 +0100

    Team backend + frontend

--- src/main/java/org/springframework/samples/petclinic/middleware/LoginInterceptor.java
+++ src/main/java/org/springframework/samples/petclinic/middleware/LoginInterceptor.java
@@ -2,16 +2,27 @@ package org.springframework.samples.petclinic.middleware;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.samples.petclinic.model.UserTW;
+import org.springframework.samples.petclinic.service.UserTWService;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    private final UserTWService userTWService;
+
+    public LoginInterceptor(UserTWService userTWService) {
+        this.userTWService = userTWService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest req,
                              HttpServletResponse res, Object handler) throws Exception {
 
         Integer userId = (Integer)req.getSession().getAttribute("userId");
-        if (userId == null)
+        // We need to query the user in case it was deleted
+        UserTW user = userTWService.findUserById(userId);
+        if (user == null)
         {
             res.sendError(403);
             return false;
--- src/main/webapp/resources/frontend/teamworks/src/components/settings/EditableField.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/settings/EditableField.js
@@ -3,10 +3,22 @@ import "../../FontStyles.css";
 import "./EditableField.css";
 import "../forms/Forms.css";
 import GradientButton from "../buttons/GradientButton";
+import { useEffect } from "react";
 
-const EditableField = ({value, inputType, smaller, editable}) => {
-  // TODO: Enter submit
+const EditableField = ({value, inputType, smaller, editable, apiFunction, fieldName}) => {
   function onOkClicked() {
+    if (!apiFunction || !fieldName)
+      return;
+    var postObj = {};
+    postObj[fieldName] = currentVal;
+    apiFunction(postObj).catch(() =>
+    {
+      setErrored(true);
+      setActualVal(actualVal); // set the old values back
+      setCurrentVal(actualVal);
+      setTimeout(() => setErrored(false), 3000);
+    });
+    setActualVal(actualVal);
     setEditing(false);
   }
 
@@ -23,17 +35,32 @@ const EditableField = ({value, inputType, smaller, editable}) => {
     }
   }
 
+  useEffect(() =>
+  {
+    if (value)
+    {
+      setCurrentVal(value);
+      setActualVal(value);
+      setLoading(false);
+    }
+  }, [value]);
+
   const [editing, setEditing] = useState(false);
+  const [actualVal, setActualVal] = useState(value);
   const [currentVal, setCurrentVal] = useState(value);
+  const [loading, setLoading] = useState(true);
   const [copied, setCopied] = useState(false);
+  const [errored, setErrored] = useState(null);
+
   if (editing) {
     return (
-      <div className={`EditableField ${smaller ? "EditableField--Smaller" : ""}`}>
+      <div className={`EditableField EditableField--Editing ${smaller ? "EditableField--Smaller" : ""}`}>
         <input
           className="Input EditingInput"
           type={inputType ?? "text"}
           defaultValue={currentVal}
           onChange={(e) => setCurrentVal(e.target.value)}
+          onKeyDown={(e) => { if (e.key == "Enter") onOkClicked() }}
         />
         <GradientButton
           className="EditingOkButton"
@@ -51,9 +78,10 @@ const EditableField = ({value, inputType, smaller, editable}) => {
     }
     return (
       <div className={`EditableField ${smaller ? "EditableField--Smaller" : ""}`} onClick={() => enterEditingMode()}>
-        <h3 className={`BoldTitle ${smaller ? "BoldTitle--Smaller" : ""}`}>{currentVal}</h3>
+        <h3 className={`BoldTitle ${smaller ? "BoldTitle--Smaller" : ""} ${loading ? "LoadingPlaceholder" : ""}`}>{currentVal}</h3>
         {penIcon}
         {copied ? <span className="CopiedSpan">Copied</span> : ""}
+        {errored ? <span className="CopiedSpan CopiedSpan--Error">Something went wrong updating the value. Try again?</span> : ""}
       </div>
     );
   }
