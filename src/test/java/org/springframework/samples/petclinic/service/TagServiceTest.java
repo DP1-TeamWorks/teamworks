3a5b9df22589fe838d0861973e54f8eeb26110ab
Fernando <frabascol@gmail.com>
2021-02-02 21:32:10 +0100

    testing negative cases

--- src/test/java/org/springframework/samples/petclinic/service/TagServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/TagServiceTest.java
@@ -1,9 +1,12 @@
 package org.springframework.samples.petclinic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,4 +63,19 @@ public class TagServiceTest {
 		assertThat(tag).isNull();
 	}
 
+	
+	
+	@Test
+	@Transactional
+	public void shouldInsertTagWithoutData() {
+
+		Project project = projectService.findProjectById(1);
+		Tag tag = new Tag();
+		tag.setProject(project);
+
+		assertThrows(ConstraintViolationException.class, ()-> {
+			tagService.saveTag(tag);
+			});			
+
+	}
 }
