528dc9a178cc3bf1ca2687e55e1eccd7d2615d72
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2021-02-06 11:37:40 +0100

    Message working good

---
+++ src/main/webapp/resources/frontend/teamworks/src/components/settings/DepartmentSettings.js
@@ -0,0 +1,72 @@
+import SettingGroup from "./SettingGroup";
+import EditableField from "./EditableField";
+import Button from "../buttons/Button";
+import AddUserForm from "../forms/AddUserForm";
+import UserList from "./DepartmentMemberList";
+import AddElementForm from "../forms/AddElementForm";
+import DepartmentsContainer from "./DepartmentsContainer";
+import AddDepartmentForm from "../forms/AddDepartmentForm";
+import { useEffect, useState } from "react";
+import DepartmentApiUtils from "../../utils/api/DepartmentApiUtils";
+import Spinner from "../spinner/Spinner";
+
+const DepartmentSettings = () =>
+{
+
+  function retrieveDepartments()
+  {
+    DepartmentApiUtils.getDepartments()
+      .then(dpts => setDepartments(dpts))
+      .catch((err) => console.error(err));
+  }
+
+  const [departments, setDepartments] = useState(null);
+  useEffect(() =>
+  {
+    retrieveDepartments();
+  }, []);
+
+  let manageDepartments;
+  if (!departments)
+  {
+    manageDepartments = (
+      <SettingGroup
+        name="Manage departments"
+        description=" ">
+          <Spinner />
+      </SettingGroup>
+    );
+  }
+  else if (departments.length > 0)
+  {
+    manageDepartments = (
+      <SettingGroup
+        name="Manage departments"
+        description="Only showing departments you have management permissions on.">
+        <DepartmentsContainer departments={departments} onDepartmentDeleted={retrieveDepartments} />
+      </SettingGroup>
+    );
+  }
+  else
+  {
+    manageDepartments = (
+      <SettingGroup
+        name="Manage departments"
+        description="There aren't any deparments yet.">
+      </SettingGroup>
+    );
+  }
+
+  return (
+    <div className="SettingGroupsContainer">
+      <SettingGroup
+        name="Create new department"
+        description="You will be able to add users once created.">
+        <AddDepartmentForm onDepartmentAdded={retrieveDepartments} />
+      </SettingGroup>
+      {manageDepartments}
+    </div>
+  );
+};
+
+export default DepartmentSettings;
---
+++ src/main/webapp/resources/frontend/teamworks/src/components/settings/UserPage.js
@@ -0,0 +1,46 @@
+import Button from "../buttons/Button";
+import GoBackButton from "../buttons/GoBackButton";
+import ProfileHeader from "../profile/ProfileHeader";
+import EditableField from "./EditableField";
+import SettingGroup from "./SettingGroup";
+import UserList from "./DepartmentMemberList";
+
+const UserPage = ({ match: { params: { userId } } }) =>
+{
+    return (
+        <>
+            <ProfileHeader
+                slim
+                src="/default_pfp.png"
+                role="Team Manager"
+                name={`User ${userId}`}>
+                <GoBackButton darker anchored />
+            </ProfileHeader>
+            <div className="SettingGroupsContainer">
+                <SettingGroup name="Name">
+                    <EditableField id="name" value="Nicolás" />
+                </SettingGroup>
+                <SettingGroup name="Last name">
+                    <EditableField id="last-name" value="de Ory Carmona" />
+                </SettingGroup>
+                <SettingGroup name="Email">
+                    <EditableField id="last-name" value="nicolasdeorycarmona@peasonspecter" editable={false} />
+                </SettingGroup>
+                <SettingGroup name="User joined">
+                    <EditableField id="join-date" value="01/10/2021" editable={false} />
+                </SettingGroup>
+                <SettingGroup name="Departments" description="A list of the departments the user belongs or has belonged to.">
+                    <UserList />
+                </SettingGroup>
+                <SettingGroup name="Projects" description="A list of the projects the user partipates or has participated in.">
+                    <UserList />
+                </SettingGroup>
+                <SettingGroup danger name="Delete user" description="Deletes the user from the team. <br>This action cannot be undone.">
+                    <Button className="Button--red">Delete user</Button>
+                </SettingGroup>
+            </div>
+        </>
+    );
+};
+
+export default UserPage;
