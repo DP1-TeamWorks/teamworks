2388ce4b9332e13b9ef4091dc1946dc370ef697a
Ignacio Navarro Blázquez <inavarroblazquez@gmail.com>
2021-02-08 17:44:50 +0100

    Milestone, message & tag service test

--- src/test/java/org/springframework/samples/petclinic/service/MilestoneServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/MilestoneServiceTest.java
@@ -4,18 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Milestone;
 import org.springframework.samples.petclinic.model.Project;
+import org.springframework.samples.petclinic.model.UserTW;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -75,6 +76,22 @@ class MilestoneServiceTest {
 		Milestone milestone2 = this.milestoneService.findNextMilestone(1);
 
 		assertThat(milestone.getDueFor()).isBefore(milestone2.getDueFor());
+	}
+	
+	
+	@Test
+	void shouldFindMilestoneForProject() {
+		Project project = projectService.findProjectById(1);
+		Collection <Milestone> milestones = this.milestoneService.findMilestonesForProject(project.getId());
+		List<Milestone> list;
+		if (milestones instanceof List)
+		  list = (List)milestones;
+		else
+		  list = new ArrayList(milestones);
+		
+		
+		assertThat(list.get(0).getName()).isEqualTo("New Year objectives");
+
 	}
 	
 	
