60f3f864ccd56e21f8786a62a47901a99cfe6137
JSnow <esoj.nieves@gmail.com>
2021-02-09 18:34:29 +0100

    Implemented attach view

--- src/main/webapp/resources/frontend/teamworks/src/components/messages/FilePreview.js
+++ src/main/webapp/resources/frontend/teamworks/src/components/messages/FilePreview.js
@@ -5,8 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLongArrowAltDown } from "@fortawesome/free-solid-svg-icons";
 
 const FilePreview = (url) => {
+  console.log(url.url);
   return (
-    <a href={url}>
+    <a href={"" + url.url} target="_blank">
       <div className="FilePreview" onClick={null}>
         <FontAwesomeIcon icon={faLongArrowAltDown} />
       </div>
