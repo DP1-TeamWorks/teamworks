146c62f1f5009927c55253bc443c1a789412e27f
Nicolás de Ory <deorynicolas@gmail.com>
2021-02-09 05:02:40 +0100

    Cascade delete project

--- src/main/java/org/springframework/samples/petclinic/service/ProjectService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ProjectService.java
@@ -3,7 +3,9 @@ package org.springframework.samples.petclinic.service;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Message;
 import org.springframework.samples.petclinic.model.Project;
+import org.springframework.samples.petclinic.model.Tag;
 import org.springframework.samples.petclinic.model.UserTW;
 import org.springframework.samples.petclinic.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProjectService {
 
     private ProjectRepository projectRepository;
+    private MessageService messageService;
 
     @Autowired
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, MessageService messageService) {
         this.projectRepository = projectRepository;
+        this.messageService = messageService;
     }
 
     @Transactional
@@ -27,6 +31,20 @@ public class ProjectService {
     @Transactional
     public void deleteProjectById(Integer projectId)
         throws DataAccessException {
+
+        Project project = projectRepository.findById(projectId);
+        if (project == null)
+            return;
+
+        for (Tag t : project.getTags())
+        {
+            for (Message m : t.getMessages())
+            {
+                m.getTags().remove(t);
+                messageService.saveMessage(m);
+            }
+        }
+
         projectRepository.deleteById(projectId);
     }
 
